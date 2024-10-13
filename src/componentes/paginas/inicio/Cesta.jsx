65916765d2b0a1e477b7acc84ab5b0f1fca1ad44
Ezequiel Alejandro Zapata <157446504+Ezequiel-A-Zapata@users.noreply.github.com>
2024-10-13 20:46:12 +0300

    mini actualizcion de estilos

--- src/componentes/paginas/inicio/Cesta.jsx
+++ src/componentes/paginas/inicio/Cesta.jsx
@@ -5,34 +5,34 @@ import "../../componentes-css/cesta.scss"
 
 function Cesta() {
 
-    const { carrito, handleSumar, handleRestar,eliminarProducto,limpiarCarrito } = useContext(CartContext)
+    const { carrito, handleSumar, handleRestar, eliminarProducto, limpiarCarrito } = useContext(CartContext)
 
-console.log(carrito)
+    console.log(carrito)
 
     return (
         <>
             <Header2></Header2>
-                        <section className='contenedor-cesta'>
-                            <button className='limpiar-carrito' onClick={limpiarCarrito}>LIMPIAR CARRITO</button>
-                        {carrito && carrito.map((prod) => {
-                            return (
-                                <div className='producto-cesta' key={prod.nombre}>
-                                    <img className='imagen-producto' src={prod.imagenes.IMG1} alt={prod.nombre} />
-                                    <div className='info-producto-cesta'>
-                                    <button className='boton-borrar' onClick={()=>{eliminarProducto(prod)}}><i className="bi bi-x"></i></button>
-                                        <p className='nombre-producto'>{prod.nombre}</p>
-                                        <p className='precio-producto'>${prod.precio}</p>
-                                        <div className='display-cantidad'>
-                                            <p>CANTIDAD: {prod.cantidad}</p>
-                                            <button onClick={() => { handleSumar(prod) }}><i className="bi bi-plus-square-fill" style={{color:"#D3D3D3"}}></i></button>
-                                            <button onClick={() => { handleRestar(prod) }}><i className="bi bi-dash-square-fill" style={{color:"#D3D3D3"}}></i></button>
-                                        </div>
-                                        <div className='descripcion-producto'>{prod.descripcion.toUpperCase()}</div>
-                                    </div>
+            <section className='contenedor-cesta'>
+                <button className='limpiar-carrito' onClick={limpiarCarrito}>LIMPIAR CARRITO</button>
+                {carrito && carrito.map((prod) => {
+                    return (
+                        <div className='producto-cesta' key={prod.nombre}>
+                            <img className='imagen-producto' src={prod.imagenes.IMG1} alt={prod.nombre} />
+                            <div className='info-producto-cesta'>
+                                <button className='boton-borrar' onClick={() => { eliminarProducto(prod) }}><i className="bi bi-x"></i></button>
+                                <p className='nombre-producto'>{prod.nombre}</p>
+                                <p className='precio-producto'>${prod.precio}</p>
+                                <div className='display-cantidad'>
+                                    <p>CANTIDAD: {prod.cantidad}</p>
+                                    <button onClick={() => { handleSumar(prod) }}><i className="bi bi-plus-square-fill" style={{ color: "#D3D3D3" }}></i></button>
+                                    <button onClick={() => { handleRestar(prod) }}><i className="bi bi-dash-square-fill" style={{ color: "#D3D3D3" }}></i></button>
                                 </div>
-                            )
-                        })}
-                    </section>
+                                <div className='descripcion-producto'>{prod.descripcion.toUpperCase()}</div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </section>
 
         </>
     )
